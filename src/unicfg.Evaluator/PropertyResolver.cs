021d4655842cff588cde9c4cda203d380856287c
duskembayev <d.uskembayev@gmail.com>
2023-03-09 15:46:06 +0200

    move find property logic to document

--- src/unicfg.Evaluator/PropertyResolver.cs
+++ src/unicfg.Evaluator/PropertyResolver.cs
@@ -6,36 +6,14 @@ namespace unicfg.Evaluator;
 public sealed class PropertyResolver : IPropertyResolver
 {
     private readonly Document _document;
-    private readonly Dictionary<PropertyRef, Property?> _properties;
 
     public PropertyResolver(Document document)
     {
         _document = document;
-        _properties = new Dictionary<PropertyRef, Property?>();
     }
-    
+
     public Property? ResolveProperty(PropertyRef propertyRef)
     {
-        if (_properties.TryGetValue(propertyRef, out var property))
-            return property;
-
-        property = _properties[propertyRef] = ResolveCore(propertyRef);
-        return property;
-    }
-
-    private Property? ResolveCore(PropertyRef propertyRef)
-    {
-        var ns = _document.RootNamespace;
-        var names = new Queue<StringRef>(propertyRef.Path);
-
-        while (ns != null && names.TryDequeue(out var name))
-        {
-            if (names.Count == 0)
-                return ns.Properties.SingleOrDefault(p => p.Name.Equals(name));
-
-            ns = ns.Namespaces.SingleOrDefault(n => n.Name.Equals(name));
-        }
-
-        return null;
+        return _document.FindProperty(propertyRef);
     }
 }
--- src/unicfg.Model/Semantic/Document.cs
+++ src/unicfg.Model/Semantic/Document.cs
@@ -1,8 +1,16 @@
-﻿namespace unicfg.Model.Semantic;
+﻿using unicfg.Model.Primitives;
+
+namespace unicfg.Model.Semantic;
 
 public class Document : ISemanticNode
 {
+    private readonly Dictionary<PropertyRef,Property?> _properties;
     private Namespace? _rootNamespace;
+
+    internal Document()
+    {
+        _properties = new Dictionary<PropertyRef, Property?>();
+    }
 
     public Namespace RootNamespace
     {
@@ -13,5 +21,30 @@ public class Document : ISemanticNode
     public void Accept(ISemanticNodeVisitor visitor)
     {
         visitor.Visit(this);
+    }
+    
+    public Property? FindProperty(PropertyRef propertyRef)
+    {
+        if (_properties.TryGetValue(propertyRef, out var property))
+            return property;
+
+        property = _properties[propertyRef] = FindPropertyCore(propertyRef);
+        return property;
+    }
+
+    private Property? FindPropertyCore(PropertyRef propertyRef)
+    {
+        var ns = RootNamespace;
+        var names = new Queue<StringRef>(propertyRef.Path);
+
+        while (ns != null && names.TryDequeue(out var name))
+        {
+            if (names.Count == 0)
+                return ns.Properties.SingleOrDefault(p => p.Name.Equals(name));
+
+            ns = ns.Namespaces.SingleOrDefault(n => n.Name.Equals(name));
+        }
+
+        return null;
     }
 }
