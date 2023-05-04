0da9bd3c627b6b7a0f07171af476b47beaedb61f
duskembayev <d.uskembayev@gmail.com>
2023-05-05 00:46:31 +0300

    add docs to primitives

--- src/unicfg.Base/Primitives/MemoryRef.cs
+++ src/unicfg.Base/Primitives/MemoryRef.cs
@@ -1,23 +1,49 @@
 ﻿namespace unicfg.Base.Primitives;
 
-internal readonly struct SegmentMemory<T> : IEquatable<ReadOnlyMemory<T>>, IEquatable<SegmentMemory<T>>
+/// <summary>
+/// Represents a memory segment that is capable of holding a reference to multiple memory segments.
+/// Supports concatenation of multiple memory segments.
+/// </summary>
+/// <typeparam name="T"> The object type from which the contiguous region of memory will be read.</typeparam>
+public readonly struct MemoryRef<T> : IEquatable<ReadOnlyMemory<T>>, IEquatable<MemoryRef<T>>
     where T : struct
 {
-    public static readonly SegmentMemory<T> Empty = default;
+    /// <summary>
+    /// The empty <see cref="MemoryRef{T}"/> instance.
+    /// Can be used to avoid unnecessary allocations.
+    /// </summary>
+    public static readonly MemoryRef<T> Empty = default;
 
     private readonly ImmutableArray<ReadOnlyMemory<T>> _segments = ImmutableArray<ReadOnlyMemory<T>>.Empty;
 
-    private SegmentMemory(ImmutableArray<ReadOnlyMemory<T>> segments)
+    private MemoryRef(ImmutableArray<ReadOnlyMemory<T>> segments)
     {
         _segments = segments;
         Length = GetTotalLength(_segments);
     }
 
+    /// <summary>
+    /// The length of the <see cref="MemoryRef{T}"/>.
+    /// </summary>
     public int Length { get; } = 0;
 
+    /// <summary>
+    /// Flag indicating whether the <see cref="MemoryRef{T}"/> is empty.
+    /// </summary>
     public bool IsEmpty => Length == 0;
+    
+    /// <summary>
+    /// Array of memory segments that this <see cref="MemoryRef{T}"/> is composed of.
+    /// </summary>
     public ImmutableArray<ReadOnlyMemory<T>> Segments => _segments;
 
+    /// <summary>
+    /// Returns the specified element of the <see cref="MemoryRef{T}"/>.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <exception cref="System.IndexOutOfRangeException">
+    /// Thrown when index less than 0 or index greater than or equal to Length
+    /// </exception>
     public T this[Index index]
     {
         get
@@ -26,7 +52,7 @@ internal readonly struct SegmentMemory<T> : IEquatable<ReadOnlyMemory<T>>, IEqua
 
             if (offset >= Length)
             {
-                throw new ArgumentOutOfRangeException(nameof(index));
+                throw new IndexOutOfRangeException("Index was outside the bounds of the SegmentMemory.");
             }
 
             var k = 0;
@@ -40,7 +66,17 @@ internal readonly struct SegmentMemory<T> : IEquatable<ReadOnlyMemory<T>>, IEqua
         }
     }
 
-    public SegmentMemory<T> this[Range range]
+    /// <summary>
+    /// Returns a new <see cref="MemoryRef{T}"/> that represents a portion of the current instance.
+    /// </summary>
+    /// <param name="range"></param>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown when start or end of the range is outside the bounds of the SegmentMemory.
+    /// </exception>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown when start of the range is greater than end of the range.
+    /// </exception>
+    public MemoryRef<T> this[Range range]
     {
         get
         {
@@ -85,16 +121,24 @@ internal readonly struct SegmentMemory<T> : IEquatable<ReadOnlyMemory<T>>, IEqua
                 break;
             }
 
-            return new SegmentMemory<T>(builder.ToImmutable());
+            return new MemoryRef<T>(builder.ToImmutable());
         }
     }
 
+    /// <summary>
+    /// Compares the current instance with the specified <see cref="ReadOnlyMemory{T}"/> for equality.
+    /// </summary>
+    /// <param name="other"></param>
     public bool Equals(ReadOnlyMemory<T> other)
     {
         return Equals(From(other));
     }
 
-    public bool Equals(SegmentMemory<T> other)
+    /// <summary>
+    /// Compares the current instance with the specified <see cref="MemoryRef{T}"/> for equality.
+    /// </summary>
+    /// <param name="other"></param>
+    public bool Equals(MemoryRef<T> other)
     {
         if (Length != other.Length)
         {
@@ -134,16 +178,23 @@ internal readonly struct SegmentMemory<T> : IEquatable<ReadOnlyMemory<T>>, IEqua
         return true;
     }
 
+    /// <summary>
+    /// Compares the current instance with the specified <see cref="object"/> for equality.
+    /// </summary>
+    /// <param name="obj"></param>
     public override bool Equals(object? obj)
     {
         return obj switch
         {
-            SegmentMemory<T> typed => Equals(typed),
+            MemoryRef<T> typed => Equals(typed),
             ReadOnlyMemory<T> typed => Equals(typed),
             _ => false
         };
     }
 
+    /// <summary>
+    /// Returns the hash code for the current instance.
+    /// </summary>
     public override int GetHashCode()
     {
         var hashCode = new HashCode();
@@ -165,7 +216,10 @@ internal readonly struct SegmentMemory<T> : IEquatable<ReadOnlyMemory<T>>, IEqua
         return hashCode.ToHashCode();
     }
 
-    public SegmentMemory<T> Concat(SegmentMemory<T> other)
+    /// <summary>
+    /// Returns a new <see cref="MemoryRef{T}"/> that is the result of concatenating the current instance with the specified <see cref="MemoryRef{T}"/>.
+    /// </summary>
+    public MemoryRef<T> Concat(MemoryRef<T> other)
     {
         if (other.IsEmpty)
         {
@@ -177,10 +231,13 @@ internal readonly struct SegmentMemory<T> : IEquatable<ReadOnlyMemory<T>>, IEqua
             return other;
         }
 
-        return new SegmentMemory<T>(_segments.AddRange(other._segments));
+        return new MemoryRef<T>(_segments.AddRange(other._segments));
     }
 
-    public SegmentMemory<T> Concat(ReadOnlyMemory<T> other)
+    /// <summary>
+    /// Returns a new <see cref="MemoryRef{T}"/> that is the result of concatenating the current instance with the specified <see cref="ReadOnlyMemory{T}"/>.
+    /// </summary>
+    public MemoryRef<T> Concat(ReadOnlyMemory<T> other)
     {
         if (other.IsEmpty)
         {
@@ -192,17 +249,22 @@ internal readonly struct SegmentMemory<T> : IEquatable<ReadOnlyMemory<T>>, IEqua
             return From(other);
         }
 
-        return new SegmentMemory<T>(_segments.Add(other));
+        return new MemoryRef<T>(_segments.Add(other));
     }
 
-    public static SegmentMemory<T> From(ReadOnlyMemory<T> memory)
+    /// <summary>
+    /// Creates a new <see cref="MemoryRef{T}"/> from the specified <see cref="ReadOnlyMemory{T}"/>.
+    /// </summary>
+    /// <param name="memory"></param>
+    /// <returns></returns>
+    public static MemoryRef<T> From(ReadOnlyMemory<T> memory)
     {
         if (memory.IsEmpty)
         {
             return Empty;
         }
 
-        return new SegmentMemory<T>(ImmutableArray.Create(memory));
+        return new MemoryRef<T>(ImmutableArray.Create(memory));
     }
 
     private static int GetTotalLength(ImmutableArray<ReadOnlyMemory<T>> array)
--- src/unicfg.Base/Primitives/StringRef.cs
+++ src/unicfg.Base/Primitives/StringRef.cs
@@ -3,38 +3,64 @@ using System.Text;
 
 namespace unicfg.Base.Primitives;
 
+/// <summary>
+/// Represents a reference to a string that is stored in a <see cref="MemoryRef{T}"/>
+/// </summary>
 public readonly struct StringRef
     : IEquatable<StringRef>,
       IEquatable<string>,
       IEquatable<ReadOnlyMemory<char>>,
       IComparable<StringRef>
 {
+    /// <summary>
+    /// Empty <see cref="StringRef"/> instance.
+    /// Can be used to avoid unnecessary allocations.
+    /// </summary>
     public static readonly StringRef Empty = default;
 
-    private readonly SegmentMemory<char> _memory = SegmentMemory<char>.Empty;
+    private readonly MemoryRef<char> _memory = MemoryRef<char>.Empty;
 
-    private StringRef(SegmentMemory<char> memory)
+    private StringRef(MemoryRef<char> memory)
     {
         _memory = memory;
     }
 
-    internal SegmentMemory<char> Memory => _memory;
+    /// <summary>
+    /// Returns memory segment that this <see cref="StringRef"/> is composed of.
+    /// </summary>
+    public MemoryRef<char> Memory => _memory;
 
+    /// <summary>
+    /// Compares with another <see cref="StringRef"/> for equality.
+    /// </summary>
+    /// <param name="other"></param>
     public bool Equals(StringRef other)
     {
         return _memory.Equals(other._memory);
     }
 
+    /// <summary>
+    /// Compares with another <see cref="ReadOnlyMemory{T}"/> for equality.
+    /// </summary>
+    /// <param name="other"></param>
     public bool Equals(ReadOnlyMemory<char> other)
     {
         return _memory.Equals(other);
     }
 
+    /// <summary>
+    /// Compares with another <see cref="string"/> for equality.
+    /// </summary>
+    /// <param name="other"></param>
     public bool Equals([NotNullWhen(true)] string? other)
     {
         return other is not null && _memory.Equals(other.AsMemory());
     }
 
+    /// <summary>
+    /// Compares with another <see cref="object"/> for equality.
+    /// </summary>
+    /// <param name="other"></param>
     public int CompareTo(StringRef other)
     {
         switch (IsEmpty, other.IsEmpty)
@@ -65,6 +91,10 @@ public readonly struct StringRef
         return Length - other.Length;
     }
 
+    /// <summary>
+    /// Compares with another <see cref="object"/> for equality.
+    /// </summary>
+    /// <param name="obj"></param>
     public override bool Equals(object? obj)
     {
         return obj switch
@@ -76,11 +106,17 @@ public readonly struct StringRef
         };
     }
 
+    /// <summary>
+    /// Returns hash code of the <see cref="StringRef"/>.
+    /// </summary>
     public override int GetHashCode()
     {
         return _memory.GetHashCode();
     }
 
+    /// <summary>
+    /// Returns string representation of the <see cref="StringRef"/>.
+    /// </summary>
     public override string ToString()
     {
         if (_memory.IsEmpty)
@@ -98,48 +134,93 @@ public readonly struct StringRef
         return builder.ToString();
     }
 
+    /// <summary>
+    /// Flag indicating whether the <see cref="StringRef"/> is empty.
+    /// </summary>
     public bool IsEmpty => _memory.IsEmpty;
 
+    /// <summary>
+    /// Length of the <see cref="StringRef"/>.
+    /// </summary>
     public int Length => _memory.Length;
 
+    /// <summary>
+    /// Returns character at the specified index.
+    /// </summary>
+    /// <param name="index"></param>
     public char this[Index index] => _memory[index];
 
+    /// <summary>
+    /// Returns substring of the <see cref="StringRef"/> at the specified range.
+    /// </summary>
+    /// <param name="range"></param>
     public StringRef this[Range range] => new(_memory[range]);
 
+    /// <summary>
+    /// Concatenates two <see cref="StringRef"/> instances.
+    /// </summary>
+    /// <param name="other"></param>
     public StringRef Concat(StringRef other)
     {
         return new StringRef(_memory.Concat(other._memory));
     }
 
+    /// <summary>
+    /// Implicitly converts <see cref="string"/> to <see cref="StringRef"/>.
+    /// </summary>
+    /// <param name="value"></param>
     public static implicit operator StringRef(string? value)
     {
         return string.IsNullOrEmpty(value)
             ? Empty
-            : new StringRef(SegmentMemory<char>.From(value.AsMemory()));
+            : new StringRef(MemoryRef<char>.From(value.AsMemory()));
     }
 
+    /// <summary>
+    /// Implicitly converts <see cref="ReadOnlyMemory{T}"/> to <see cref="StringRef"/>.
+    /// </summary>
+    /// <param name="value"></param>
     public static implicit operator StringRef(ReadOnlyMemory<char> value)
     {
         return value.IsEmpty
             ? Empty
-            : new StringRef(SegmentMemory<char>.From(value));
+            : new StringRef(MemoryRef<char>.From(value));
     }
 
+    /// <summary>
+    /// Explicitly converts <see cref="StringRef"/> to <see cref="string"/>.
+    /// </summary>
+    /// <param name="value"></param>
     public static explicit operator string(StringRef value)
     {
         return value.ToString();
     }
 
+    /// <summary>
+    /// Compares two <see cref="StringRef"/> instances.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
     public static bool operator ==(StringRef left, StringRef right)
     {
         return left.Equals(right);
     }
 
+    /// <summary>
+    /// Compares two <see cref="StringRef"/> instances.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
     public static bool operator !=(StringRef left, StringRef right)
     {
         return !(left == right);
     }
 
+    /// <summary>
+    /// Concatenates two <see cref="StringRef"/> instances.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
     public static StringRef operator +(StringRef left, StringRef right)
     {
         return left.Concat(right);
